6dab23c9f83ed3c34f426a098a6f92eeac06c5e3
NorthEastKnow <madhushudhanmondal1234@gmail.com>
2022-07-01 14:04:10 +0530

    helo

--- push.js
+++ push.js
@@ -7,7 +7,14 @@ let vapidKeys ={
 
 push.setVapidDetails('mailto:test@test.co.uk', vapidKeys.publicKey,vapidKeys.privateKey);
 
-let sub = {};
+let sub = {
+    endpoint:"https://fcm.googleapis.com/fcm/send/fQ0Ij4xa-h8:APA91bH8GoxHvkyiUsjvKgEU2sdxaebPE0S-nlll7uLUfCkJyB2lUT1vZcE-rC_LHrYZaRB99DaLmWU_38zFjrrWztLZr8rDhOJn2R27WTSSILDAi4u5vFGD2kkq50SqeRqMUHG5ui74",
+    expirationTime:null,
+    keys: { 
+        "p256dh":"BNYlZ44r5lRcjkT0e2kinqo9Gy3Qd5N9TvbuosxF2aK0K8NXe5dvjtcgU_3s3EoO0_KQgsXMCPjwg_C5pSaowt4",
+        auth:"nqUt8nFQoPJwjQ8LEQ3xkA"
+    }
+};
 
 push.sendNotification(sub, 'test message');
 
