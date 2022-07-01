2aac928a46f31a50dc2a14721b99e5af678c5132
NorthEastKnow <madhushudhanmondal1234@gmail.com>
2022-07-01 13:45:15 +0530

    fix

--- sw.js
+++ sw.js
@@ -1,3 +1,49 @@
-self.addEventListener('push',() => {
-    self.registration.sendNotification('test message', {});
+// self.addEventListener('push',() => {
+//     self.registration.sendNotification('test message', {});
+// })
+// self.addEventListener('push', fuction(e) {
+//     var : options = {
+//         body: 'This notification was generated from a push!',
+//         icon: 'images/example.png',
+//         vibrate: [100,50,100],
+//         date: {
+//             dateOfArrival: date.now(),
+//             primaryKey: '2'
+//         },
+//         actions: [
+//             {
+//                 action: 'explore',title: 'Explore this new world',
+//                 icon: 'images/checkmark.png'
+//             },
+//             {
+//                 action: 'close', title: 'Close',
+//                 icon: 'images/xmark.png'
+//             },
+//         ]
+            
+//     },
+    
+// });
+self.addEventListener('push' , function(e){
+var options = {
+    body: 'This notification was generated from a push!',
+    icon: 'images/example.png',
+     vibrate: [100,50,100],
+     date: {
+             dateOfArrival: date.now(),
+               primaryKey: '2'
+              },
+
+              actions: [
+                 {
+                action: 'explore',title: 'Explore this new world',
+                icon: 'images/checkmark.png'
+                 },
+                 {
+                action: 'close', title: 'Close',
+                 icon: 'images/xmark.png'
+                 },
+                ]
+     };
+     e.waitUntil(self.ServiceWorkerRegistration.showNotification('Hello World',options));
 });
